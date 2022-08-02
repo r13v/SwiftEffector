eebf8812c2c69e914fade258204782637b21c4a3
Rustam Gilyaziev <gilaziev@gmail.com>
2022-08-02 16:57:39 +0300

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# SwiftEffector
+
+A description of this package.
---
+++ Tests/SwiftEffectorTests/SwiftEffectorTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import SwiftEffector
+
+final class SwiftEffectorTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(SwiftEffector().text, "Hello, World!")
+    }
+}
