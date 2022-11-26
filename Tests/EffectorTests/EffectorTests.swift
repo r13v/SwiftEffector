3e1c9f75a2af42d353550ca5855544a8520b3d09
Rustam Giliaziev <gilaziev@gmail.com>
2022-11-26 08:48:05 -0300

    test: check computed

--- Tests/EffectorTests/EffectorTests.swift
+++ Tests/EffectorTests/EffectorTests.swift
@@ -638,4 +638,20 @@ final class EffectorTests: XCTestCase {
 
         XCTAssert(c.getState())
     }
+
+    func testComputed() async throws {
+        let entry = Event<Int>()
+        let a = Store(1).on(entry) { _, v in v }
+        let b = a.map { $0 + 1 }
+        let c = a.map { $0 + 1 }
+        let d = combine(b, c) { $0 + $1 }
+        let e = d.map { $0 + 1 }
+        let f = combine(d, e) { $0 + $1 }
+        let g = combine(d, e) { $0 + $1 }
+        let h = combine(f, g) { $0 + $1 }
+
+        entry(1)
+
+        XCTAssertEqual(h.getState(), 18)
+    }
 }
